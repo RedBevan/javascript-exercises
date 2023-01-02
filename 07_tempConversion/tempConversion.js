8339fc351894bf99c1eca2960232abbe7337e4bf
Joe <jrbevan537@gmail.com>
2023-01-02 16:33:37 +0100

    Solve to ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFar) {
+  let tempInCels = ((inputFar - 32)*0.5556);
+  return Math.round(tempInCels * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCels) {
+  let tempInFar = (inputCels*1.8) + 32;
+  return Math.round(tempInFar * 10) / 10;
 };
+
+// to convert far to cels, subtract 32 and x by .5556
+
+// to convert c to f, x by 1.8 and add 32
 
 // Do not edit below this line
 module.exports = {
