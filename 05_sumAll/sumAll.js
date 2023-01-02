8077d0441bd20e75daae3e3fa06167a43f31fa2e
Joe <jrbevan537@gmail.com>
2023-01-02 15:32:47 +0100

    Solve to ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(x, y) {
   let sumTotal = 0;
-  if ( x < 0 || y < 0 || isNaN(x) || isNaN(y) ) {
+  if ( x < 0 || y < 0 || typeof x !== 'number' || typeof y !== 'number' ) {
     return 'ERROR';
   } else {
   if (x>y) {
