ca8513cf103a12627096350b0829ec7717cca623
Joe <jrbevan537@gmail.com>
2023-01-02 15:18:19 +0100

    Solve to ex5, test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sumTotal = 0;
+  if ( x < 0 || y < 0 || isNaN(x) || isNaN(y) ) {
+    return 'ERROR';
+  } else {
+  if (x>y) {
+  for (i=y; i <= x; i++) {
+    sumTotal += i;
+  }
+  return sumTotal;
+  } else {
+  for (i=x; i <= y; i++) {
+    sumTotal += i;
+  }
+  return sumTotal;
+}
+}
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
