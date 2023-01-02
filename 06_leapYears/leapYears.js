28d521df476eb33b67835f398285329ff15f7e6e
Joe <jrbevan537@gmail.com>
2023-01-02 16:17:12 +0100

    Solve to ex 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
-};
+const leapYears = function(x) {
+  if (x%4 != 0) {
+    console.log('not divisible by 4');
+    return false;
+  } else if (x%100 === 0) {
+    console.log('divisible by 100');
+      if (x%400 === 0) {
+        console.log('divisible by 400');
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      console.log('not divisible by 100');
+      return true;
+    }
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
